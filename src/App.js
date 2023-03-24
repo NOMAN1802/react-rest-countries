1f300779de15ac2aa6edcdf9e850f21c84100bac
NOMAN1802 <mustakimnoman82@gmail.com>
2023-03-24 10:46:54 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Countries from './components/Countries/Countries';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
      <Countries></Countries>
+     <Footer></Footer>
       
     </div>
   );
@@ -31,7 +35,8 @@ function Country(props){
  return(
   <div>
     <h2>Name: {props.name} </h2>
+    <h4>Population: {props.population}</h4>
   </div>
  )
-}
+} */
 export default App;
